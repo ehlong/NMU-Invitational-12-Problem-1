d0a67f5aaaefa8fab38ae8d1a23804152768dba2
elliott.long <elliott.long@wsu.edu>
2020-03-09 20:38:31 -0700

    ADDED LOOPS
    Loops added to build the rings and core

--- lab_python.py
+++ lab_python.py
@@ -2,9 +2,22 @@
 rings = int(input("Rings: "))
 core = int(input("Core Size: "))
 # collected for forming rings/cores
+ringu = rings       # dummy value used to manipulate
 
-# nest loops to generate shapes, these generally work
-print(' ' * (rings - 1), end='')
-print('/', end='')
-print('-' * core, end='')
-print(r"\ ")
+for x in range(0, rings):   # loop for upper part
+    ringu = ringu - 1
+    print(' ' * ringu, end='')
+    print('/' * (x + 1), end='')
+    print('-' * core, end='')
+    print("\\" * (x + 1))
+for x in range(0, core):    # loop for middle
+    print("|" * rings, end='')
+    print(' ' * core, end='')
+    print("|" * rings)
+ringu = rings   # resetting the ringu value
+for x in range(0, rings):   # loop for bottom
+    print(' ' * x, end='')
+    print("\\" * ringu, end='')
+    print('-' * core, end='')
+    print('/' * ringu)
+    ringu = ringu - 1
