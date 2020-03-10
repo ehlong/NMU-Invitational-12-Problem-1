19ffb26acf0b44cdc09745a311da71ac9198b515
elliott.long <elliott.long@wsu.edu>
2020-03-09 20:52:00 -0700

    CONDITIONAL EXIT
    Added an exit condition so it runs multiple times if desired

--- lab_python.py
+++ lab_python.py
@@ -1,23 +1,26 @@
 # Elliott Long
-rings = int(input("Rings: "))
-core = int(input("Core Size: "))
-# collected for forming rings/cores
-ringu = rings       # dummy value used to manipulate
+rings = 1
+core = 1
+while rings != 0 or core != 0:      # loop, choose 0 0 to exit
+    rings = int(input("Rings: "))
+    core = int(input("Core Size: "))
+    # collected for forming rings/cores
+    ringu = rings       # dummy value used to manipulate
 
-for x in range(0, rings):   # loop for upper part
-    ringu = ringu - 1
-    print(' ' * ringu, end='')
-    print('/' * (x + 1), end='')
-    print('-' * core, end='')
-    print("\\" * (x + 1))
-for x in range(0, core):    # loop for middle
-    print("|" * rings, end='')
-    print(' ' * core, end='')
-    print("|" * rings)
-ringu = rings   # resetting the ringu value
-for x in range(0, rings):   # loop for bottom
-    print(' ' * x, end='')
-    print("\\" * ringu, end='')
-    print('-' * core, end='')
-    print('/' * ringu)
-    ringu = ringu - 1
+    for x in range(0, rings):   # loop for upper part
+        ringu = ringu - 1
+        print(' ' * ringu, end='')
+        print('/' * (x + 1), end='')
+        print('-' * core, end='')
+        print("\\" * (x + 1))
+    for x in range(0, core):    # loop for middle
+        print("|" * rings, end='')
+        print(' ' * core, end='')
+        print("|" * rings)
+    ringu = rings   # resetting the ringu value
+    for x in range(0, rings):   # loop for bottom
+        print(' ' * x, end='')
+        print("\\" * ringu, end='')
+        print('-' * core, end='')
+        print('/' * ringu)
+        ringu = ringu - 1
